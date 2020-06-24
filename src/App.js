dff88c4919fba34b03cb31dba55c3ad91c92e548
Samantha Gagen <sgagen@r1rcm.com>
2020-06-24 08:28:35 -0600

    Add style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const style = {
+  margin: "0 auto",
+  width: "640px",
+};
+
 function App() {
   return (
-    <div>
-      <header>
-        <h1> My todo app</h1>>
+    <div style={style}>
+      <header className="big-font">
+        <Title title="My TODO App" />
       </header>
     </div>
   );
 }
 
 export default App;
+
+function Title(props) {
+  return <h1>{props.title}</h1>;
+}
